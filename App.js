e4161c84e367e2e147b8434552a58b45fb00a526
James Dowe <vgkid1@gmail.com>
2018-01-09 10:19:17 -0700

    auth

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase'
-import { Header } from './src/components/common'
+import { Header, Button, Spinner } from './src/components/common'
 import LoginForm from './src/components/LoginForm.js'
 
 export default class App extends Component {
+  state={loggedIn: null}
+
   componentWillMount(){
     firebase.initializeApp({
     apiKey: "AIzaSyAKGRxarG9SQFhzoEKKvFB9vK2aAEZxaJA",
@@ -14,13 +16,36 @@ export default class App extends Component {
     storageBucket: "health-tracker-zephyum.appspot.com",
     messagingSenderId: "699378839717"
   })
+  firebase.auth().onAuthStateChanged((user) => {
+    if(user){
+      this.setState({loggedIn: true})
+    } else {
+    this.setState({loggedIn: false})
+  }
+  })
 }
+
+renderContent() {
+  switch (this.state.loggedIn) {
+    case true:
+      return (
+      <Button onPress={() => firebase.auth().signOut()}>
+      Log Out
+      </Button>
+    )
+    case false:
+      return <LoginForm />
+    default:
+      return <Spinner size="large" />
+  }
+}
+
 
   render() {
     return (
       <View>
       <Header headerText="Authentication!"/>
-      <LoginForm />
+      {this.renderContent()}
       </View>
     );
   }
